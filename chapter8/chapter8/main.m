a9154a7e58ff2cba0784d94ca7710ee254a44d70
YU WEI <vic.weiyu@gmail.com>
2020-03-13 17:23:00 +1030

    chapter8 init

---
+++ chapter8/chapter8/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chapter8
+//
+//  Created by Yu WEI on 10/3/20.
+//  Copyright © 2020 Victor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
